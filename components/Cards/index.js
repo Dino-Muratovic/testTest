d2b6760603d9bdfbaba083cbc64c86f89ff09077
Dino-Muratovic <dinomuratovic1989@gmail.com>
2020-03-13 12:50:56 -0500

    I was stuck during the sprint challenge and will have to retake it

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -8,12 +8,67 @@
 //
 // <div class="card">
 //   <div class="headline">{Headline of article}</div>
+
 //   <div class="author">
 //     <div class="img-container">
 //       <img src={url of authors image} />
 //     </div>
+
 //     <span>By {authors name}</span>
 //   </div>
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response =>{
+//   console.log(`this is response `, response);  
+//   console.log(`this is javascript `, response.data.articles.javascript); 
+//   console.log(`this is javascript `, response.data.javascript); 
+
+response.data.articles.javascript.forEach(info =>{
+//   console.log(`this is info `, info);
+    newCard.append(createCard(info));
+    // console.log(`this is new card `, newCard);
+})  
+
+
+
+}).catch(error => {
+    console.log("This is the error", error)
+})
+
+
+function createCard(object){
+
+    //create and define elements
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const image = document.createElement('img');
+    const authorName = document.createElement('span');
+
+    // add classes if any
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    //append here
+    card.append(headline, author, authorName);
+    author.append(imgContainer);
+    imgContainer.append(image);
+
+    //text content here
+    headline.textContent = object.headline;    
+    image.src = object.authorPhoto;
+    authorName.textContent = object.authorName;
+
+
+    return card;
+}
+
+let newCard = document.querySelector('.cards-container');
+
+
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,31 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then(response =>{
+    // console.log(`this is topics `, response.data.topics);
+    response.data.topics.forEach(index =>{
+        // console.log(`this is index `, index);
+        newTab.append(newTabComponent(index));
+        // console.log(`this is index `, index);
+    })
+
+})
+
+.catch(error => {
+    console.log("This is the error", error)
+})
+
+function newTabComponent (data){
+    const tab = document.createElement('div');
+    tab.classList.add('tab');
+    tab.textContent = data. topics;   
+
+
+    return tab;
+}
+
+const newTab = document.querySelector('.topics');
+// newTab.append();
