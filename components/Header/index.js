575870f543128df901c1e05cb16f7446f3e48c57
Dino-Muratovic <dinomuratovic1989@gmail.com>
2020-03-14 14:58:29 -0500

    starting from scratch the re practice of applied JS sprint

--- components/Header/index.js
+++ components/Header/index.js
@@ -10,31 +10,5 @@
 // And add it to the DOM in the .header-container component
 
 function Header() {
-//create and define elements
-const header = document.createElement('div');
-const date = document.createElement('span');
-const title = document.createElement('h1');
-const temp = document.createElement('span');
 
-//add classes if any
-header.classList.add('header'); 
-date.classList.add('date');
-temp.classList.add('temp');
-
-// add text content here
-date.textContent = "SMARCH 28, 2019";
-title.textContent = "Lambda Times";
-temp.textContent = "98°";  
-
-//append elements here
-header.append(date, title, temp);
-
-return header;
-}
-
-//grab the parent element from html and append the create header
-const headerContainer = document.querySelector('.header-container');
-headerContainer.append(Header());
-
-// console.log(headerContainer);
 
